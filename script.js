a1fac139102d6dc73f524266e007b006f97698b7
Uv-Katalist <orinahyuvinalismogeni@gmail.com>
2021-04-14 12:09:30 +0300

    service background

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ $(document).ready(function(){
 
     // owl carousel script
     $('.carousel').owlCarousel({
-        margin: 20,
+        margin: 30,
         loop: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
